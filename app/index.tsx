fc743ef5b4bfa8dd6c4f95f9e23f4a522047b766
Md. Abdullah Al Hasan <aahassan.6898@gmail.com>
2025-05-27 16:13:50 +0600

    day 1

---
+++ app/index.tsx
@@ -0,0 +1,29 @@
+import * as SplashScreen from "expo-splash-screen";
+import React, { useEffect } from "react";
+import { Text, View, Button } from "react-native";
+
+SplashScreen.preventAutoHideAsync(); // Prevent it from auto-hiding
+
+export default function Index() {
+  useEffect(() => {
+    const prepare = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1500)); // Delay for 2.5 seconds
+      await SplashScreen.hideAsync(); // Hide splash screen manually
+    };
+    prepare();
+  }, []);
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#C62C2C",
+      }}
+    >
+      <View >
+        <Text> Go to Home Page </Text>
+      </View>
+    </View>
+  );
+}
